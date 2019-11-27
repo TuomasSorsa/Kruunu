f54d330f6570a24576ea8c518decfe3a6a7f31e0
Tuomas Sihvo <tuomas.sihvo@metropolia.fi>
2019-11-27 09:00:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kruunu/kalenteriMenu.java
@@ -0,0 +1,14 @@
+package com.example.kruunu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kalenteriMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalenteri_menu);
+    }
+}
---
+++ app/src/main/java/com/example/kruunu/pesuMenu.java
@@ -0,0 +1,14 @@
+package com.example.kruunu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pesuMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesu_menu);
+    }
+}
