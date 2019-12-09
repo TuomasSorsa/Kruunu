747f4650b014eb552db00b068f6b29e2f57dcfb7
Tuomas Sihvo <tuomas.sihvo@metropolia.fi>
2019-12-09 10:59:29 +0200

    player tulossa

--- app/src/main/java/com/example/kruunu/PesuMenu.java
+++ app/src/main/java/com/example/kruunu/PesuMenu.java
@@ -2,13 +2,26 @@ package com.example.kruunu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
-public class PesuMenu extends AppCompatActivity {
+public class  PesuMenu extends AppCompatActivity {
+    MediaPlayer pesuplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesu_menu);
+    }
+
+    public void Play (View v) {
+        if(pesuplayer == null) {
+            pesuplayer = MediaPlayer.create(this, R.raw.cinderella)
+        }
+    }
+
+    public void Pause (View v) {
+
     }
 }
